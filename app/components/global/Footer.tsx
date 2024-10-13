2b8445c694c27bce81c6adad884b7aeb32e4cadf
desy scott <Mrfallback055@gmail.com>
2024-10-13 19:32:28 +0000

    fix:modify footer

--- app/components/global/Footer.tsx
+++ app/components/global/Footer.tsx
@@ -75,7 +75,7 @@ export default function Footer() {
             </a> */}
 
             <small className="text-zinc-500">
-              Copyright &copy; Victor Eke {new Date().getFullYear()} All rights
+              Copyright &copy; Desy Scott {new Date().getFullYear()} All rights
               Reserved
             </small>
           </div>
